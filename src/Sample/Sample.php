ada384d9cc7bf80b06d7bd65486582da4550170e
Taro Hida <sk8trou@gmail.com>
2020-05-27 22:59:42 +0900

    create unit test (red)

---
+++ src/Sample/Sample.php
@@ -0,0 +1,11 @@
+<?php
+namespace tarospace\Sample;
+
+class Sample
+{
+    public static function alphabet(int $input): string
+    {
+        $alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        return $alphabet[$input -1];
+    }
+}
